3fabd11d7c85c00f355b86708d11c0b01f945ba1
rythmstories <gauravgupta1051996@gmail.com>
2021-02-08 12:02:56 +0530

    heroku deploy 11

--- server1/urls.py
+++ server1/urls.py
@@ -4,13 +4,13 @@ from .views import createtoken, getalltoken, tokenpool, assigntoken, getuserdeta
 
 urlpatterns = [
 
-    url(r'token/', createtoken, name="Bank"),				# create token (post), delete all token (put), delete specific token (put)
-    url(r'all/', getalltoken, name="Bank"),					# (post) token, to check details / live link for any token / delete the token if it is not updated in 5 min
-    url(r'pool/', tokenpool, name="Bank"),					# to see pool of tokens
-    url(r'assign/', assigntoken, name="Bank"),				# to see free token (GET), assign free token (post) and check whether token exist for same credential
+    url(r'token/', createtoken, name="test"),				# create token (post), delete all token (put), delete specific token (put)
+    url(r'all/', getalltoken, name="test"),					# (post) token, to check details / live link for any token / delete the token if it is not updated in 5 min
+    url(r'pool/', tokenpool, name="test"),					# to see pool of tokens
+    url(r'assign/', assigntoken, name="test"),				# to see free token (GET), assign free token (post) and check whether token exist for same credential
     														# to unblock a token (PUT)
 
-    url(r'user/', getuserdetails, name="Bank"),				# give all users (get), to get user details (post), delete a user(put) 
+    url(r'user/', getuserdetails, name="test"),				# give all users (get), to get user details (post), delete a user(put) 
 
 ]
 
